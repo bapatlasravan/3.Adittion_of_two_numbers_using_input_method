4bba3f62c2379ea37ca2fbf6ff065fec8f0f2140
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-08-05 10:25:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.addition
+{
+    class Program
+    {
+        public void ADD(int a,int b)
+        {
+            int c=a+b;
+            Console.WriteLine("sum of {0} and {1} is {2} :- ",a, b, c);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("enter the first numbers ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second numbers ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            obj.ADD(a,b);
+
+
+        }
+    }
+}
